689acc821358c877cbd0aa3d8a8e9c95a9319a08
DCox2016 <leadsingerstarke@yahoo.com>
2019-04-24 14:06:11 -0400

    Adds sign up and sign in

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'ZKOTW'`, () => {
+  it(`should have as title 'zombie-kill-of-the-week'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('ZKOTW');
+    expect(app.title).toEqual('zombie-kill-of-the-week');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to ZKOTW!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to zombie-kill-of-the-week!');
   });
 });
